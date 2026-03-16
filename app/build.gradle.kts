1d292607d264bed4327d1086b994aa5eeaab33d8
owo233 <ver@vip.qq.com>
2026-03-16 16:29:37 +0800

    bump version 1.2.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.owo233.fuckmarketads"
         minSdk = 31
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 25
+        versionName = "1.2.5"
         buildConfigField("String", "APP_NAME", "\"Fuck Market Ads\"")
     }
 
